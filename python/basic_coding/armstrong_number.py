cbc075fbc81edb482cab13f520b6d89b3589b129
vikrantbachhav <appsvikrant@gmail.com>
2024-02-19 21:33:54 -0800

    armstrong number

---
+++ python/basic_coding/armstrong_number.py
@@ -0,0 +1,19 @@
+import math
+def check_armstrong_num(n):
+    tmp = n
+    order = 0
+
+    while(tmp > 0):
+        tmp = tmp // 10
+        order = order + 1
+
+    tmp = n
+    sum = 0
+    for i in range(tmp - 1):
+        sum = sum + math.pow(tmp % 10, order)
+        tmp = tmp // 10
+
+    return (sum == n)
+
+if __name__ == '__main__':
+    print(check_armstrong_num(153))
