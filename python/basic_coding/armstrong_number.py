e3fe1e89f6c78ecc26a47a0e2639965d161ca7b0
vikrantbachhav <appsvikrant@gmail.com>
2024-02-19 21:47:40 -0800

    armstrong number pythonic version

--- python/basic_coding/armstrong_number.py
+++ python/basic_coding/armstrong_number.py
@@ -10,7 +10,7 @@ def check_armstrong_num(n):
     tmp = n
     sum = 0
     for i in range(tmp - 1):
-        sum = sum + math.pow(tmp % 10, order)
+        sum = sum + math.pow(tmp % 10, order) #alternate pythonic solution is sum = sum + (tmp % 10) ** order
         tmp = tmp // 10
 
     return (sum == n)
