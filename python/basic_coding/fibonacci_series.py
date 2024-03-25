cf2cd5971eda18adc188996e1e0b8d96ce3a3b6c
Vikrant Bachhav <vikrant.bachhav@vikrant.bachhav-17SM>
2024-03-24 17:31:52 -0700

    fibo 2 solutions

--- python/basic_coding/fibonacci_series.py
+++ python/basic_coding/fibonacci_series.py
@@ -5,5 +5,16 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-n = 5 #0,1,1,2,3,5,8,13..
-print(fib(n))
+def fibo(n):
+    if n <= 1:
+        return n
+    a,b = 0,1
+    for i in range(n-1):
+        c = a + b
+        a = b
+        b = c
+    return c
+
+# Fibonacci series - Zero indexed, starting with 0: 0,1,1,2,3,5,8,13..
+print(fib(7))
+print(fibo(7))
